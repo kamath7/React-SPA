50cb9ea6bf90b29a0bc42652b306267532e3ec41
kamath7 <adithyakamath96@gmail.com>
2021-09-10 20:33:09 +0530

    highlighting the quote

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,12 +1,22 @@
 import React from "react";
 import { Route, useParams } from "react-router";
 import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Kams", text: "Rhea stupid" },
+  { id: "q2", author: "Somebody", text: "Pujit PS" },
+  { id: "q3", author: "God", text: "Dhoni is the GOAT" },
+];
 const QuoteDetail = () => {
   const params = useParams();
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+  if(!quote){
+      //if no quote
+      <p>No quote found!</p>
+  }
   return (
     <React.Fragment>
-      <h1>To view single quote</h1>
-      <p>{params.quoteId}</p>
+      <HighlightedQuote text={quote.text} author={quote.author} />
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
