24d1ccff1e6d478700ae8f5a011e652125b32726
kamath7 <adithyakamath96@gmail.com>
2021-09-10 22:01:47 +0530

    using useHistory to navigate

--- src/pages/AddQuote.js
+++ src/pages/AddQuote.js
@@ -1,8 +1,11 @@
 import React from "react";
+import { useHistory } from "react-router";
 import QuoteForm from "../components/quotes/QuoteForm";
 const AddQuote = () => {
+  const history = useHistory();
   const addQuote = (quoteData) => {
     console.log(quoteData);
+    history.push("/quotes");
   };
   return <QuoteForm onAddQuote={addQuote} />;
 };
