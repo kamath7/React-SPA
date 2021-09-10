acf7c6492c62825530ca0ac7aea0a5dd50f19402
kamath7 <adithyakamath96@gmail.com>
2021-09-10 20:03:04 +0530

    nested routing to include comments

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,15 +1,17 @@
-import React from 'react'
-import { useParams } from 'react-router'
-
+import React from "react";
+import { Route, useParams } from "react-router";
+import Comments from "../components/comments/Comments";
 const QuoteDetail = () => {
-const params = useParams()
-    return (
-        <React.Fragment>
-            <h1>To view single quote</h1>
-            <p>{params.quoteId}</p>
+  const params = useParams();
+  return (
+    <React.Fragment>
+      <h1>To view single quote</h1>
+      <p>{params.quoteId}</p>
+      <Route path={`/quotes/${params.quoteId}/comments`}>
+        <Comments />
+      </Route>
+    </React.Fragment>
+  );
+};
 
-        </React.Fragment>
-    )
-}
-
-export default QuoteDetail
+export default QuoteDetail;
